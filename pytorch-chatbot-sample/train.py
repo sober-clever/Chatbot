4eb417dee8ceb073e05a73657305d759bbe79f62
Sober_Clever <Sober_Clever@mail.ustc.edu.cn>
2022-05-13 20:37:10 +0800

    Add plan

--- pytorch-chatbot-sample/train.py
+++ pytorch-chatbot-sample/train.py
@@ -62,6 +62,7 @@ hidden_size = 8
 output_size = len(tags)
 print(input_size, output_size)
 
+
 class ChatDataset(Dataset):
 
     def __init__(self):
@@ -115,12 +116,12 @@ for epoch in range(num_epochs):
 print(f'final loss: {loss.item():.4f}')
 
 data = {
-"model_state": model.state_dict(),
-"input_size": input_size,
-"hidden_size": hidden_size,
-"output_size": output_size,
-"all_words": all_words,
-"tags": tags
+    "model_state": model.state_dict(),
+    "input_size": input_size,
+    "hidden_size": hidden_size,
+    "output_size": output_size,
+    "all_words": all_words,
+    "tags": tags
 }
 
 FILE = "data.pth"
